3e1a156a4df6c6f65e4c14050054f1144c81b0e2
mengweetan <mengwee@dapoltd.com>
2020-03-11 19:10:47 +0800

    train file

--- train/train.py
+++ train/train.py
@@ -20,3 +20,40 @@ HAIKU_LINES_NUM = 3
 
 df = pd.read_csv(dataDir+'__INPUT.txt', sep = '\t')
 df.info()
+
+df = df[:100]
+t = Tokenizer()
+t.fit_on_texts([df['input_texts'][i] for i in range(df.shape[0])])
+vocab_size = len(t.word_index) + 1 # note - padded 1
+
+encoded_docs = t.texts_to_sequences([df['input_texts'][i] for i in range(df.shape[0])])
+max_len = max([len(l) for l in encoded_docs]) # get the max len
+padded_docs = pad_sequences(encoded_docs, maxlen=max_len, padding='post')
+
+embeddings_index = dict()
+f = open(dataDir+'glove.6B/glove.6B.100d.txt') # try 100 dimension
+for line in f:
+	values = line.split()
+	word = values[0]
+	coefs = np.asarray(values[1:], dtype='float32')
+	embeddings_index[word] = coefs
+f.close()
+print('Loaded %s word vectors.' % len(embeddings_index))
+
+embedding_matrix = np.zeros((vocab_size, 100)) # because we are using 100 dimension pre trained embedding
+for word, i in t.word_index.items():
+	embedding_vector = embeddings_index.get(word)
+	if embedding_vector is not None:
+		embedding_matrix[i] = embedding_vector
+
+print (embedding_matrix)
+print (type(embedding_matrix))
+print (embedding_matrix.shape)
+
+from numpy import savetxt, loadtxt
+savetxt(dataDir+'embedding_matrix.csv', embedding_matrix, delimiter=',')
+
+
+embedding_matrix = numpy.loadtxt(open(dataDir+'embedding_matrix.csv', "rb"), delimiter=",", skiprows=1)
+print (type(embedding_matrix))
+print (embedding_matrix.shape)
