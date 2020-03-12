4700ed18c992b875aba375e6d859c87d8112c7e6
mengweetan <mengwee@dapoltd.com>
2020-03-12 17:45:28 +0800

    p

--- deploy/infer.py
+++ deploy/infer.py
@@ -1,25 +1,167 @@
-from flask import Flask, jsonify
-from sklearn.externals import joblib
+import tensorflow as tf
+
+import numpy as np
+import pandas as pd
+import os
+
+Model = tf.keras.models.Model
+Input = tf.keras.layers.Input
+LSTM = tf.keras.layers.LSTM
+Embedding = tf.keras.layers.Embedding
+Dense = tf.keras.layers.Dense
+concatenate = tf.keras.layers.concatenate
+Flatten = tf.keras.layers.Flatten
+Add =  tf.keras.layers.Add
+
+pad_sequences = tf.keras.preprocessing.sequence.pad_sequences
+Tokenizer = tf.keras.preprocessing.text.Tokenizer
+
+seed ='some random keywords'
+#dataDir='./data/'
+dataDir='data/'
+
+
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-m", "--model", dest="model",
-                    help="location of the pickle file")
 
-filename = parser.parse_args().model
+parser.add_argument("-s", "--seed",dest="seed",
+                    help="seed keywords"
+                    )
 
-app = Flask(__name__)
+seed = parser.parse_args().seed
 
-@app.route('/')
-def index():
-    return "Stackoverflow Salary Predictor"
 
-@app.route('/sal/<int:x>', methods=['GET'])
-def predict(x):
-	loaded_model=joblib.load(filename)
-	y=loaded_model.predict([[x]])[0]
-	sal=jsonify({'salary': round(y,2)})
-	return sal
 
-if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=8080)
+
+
+
+t = Tokenizer()
+t.fit_on_texts([seed])
+t.word_index
+
+
+encoded_docs = t.texts_to_sequences([seed])
+max_len = len(encoded_docs[0])
+
+embedding_matrix = np.loadtxt(open(dataDir+'embedding_matrix.csv', "rb"), delimiter=",")
+print (embedding_matrix.shape)
+
+latent_dim = 50 # note using 50 dim GLOVE
+HAIKU_LINES_NUM = 3
+
+load_model = tf.keras.models.load_model
+#model = load_model('model/haiku/modelv2-1.h5',custom_objects={'tf': tf}, compile=False)
+
+# TEMP CODES
+num_encoder_tokens = 50000
+num_decoder_tokens = 50000
+
+inputs = Input(shape=(None,))
+aux_inputs = [Input(shape=(None,), name='haikuLine_{}'.format(i)) for i in range(HAIKU_LINES_NUM)]
+syllabus_inputs = [Input(shape=(1,),  dtype='int32', name='syllabus_input_{}'.format(i)) for i in range(HAIKU_LINES_NUM)]
+
+x =  Embedding(num_encoder_tokens+1, latent_dim, mask_zero = True)(inputs)
+_ , state_h, state_c = LSTM(latent_dim,  return_state=True) (x)
+
+encoder_model = Model([inputs, syllabus_inputs[0],syllabus_inputs[1],syllabus_inputs[2]], [state_h, state_c])
+
+'''
+model = load_model('model/haiku/modelv2-2.h5')
+encoder_model = Model( [ model.inputs[0], model.inputs[4], model.inputs[5], model.inputs[6]], [model.layers[4].output[1],model.layers[4].output[2] ])
+'''
+
+#decoder model
+
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+
+
+syllabus_state_hs = []
+syllabus_state_cs = []
+syllabus_dense = []
+
+last_states_hs = []
+last_states_cs = []
+
+decoder_outputs2 = []
+
+
+for i in range(self.HAIKU_LINES_NUM):
+
+    x2 = Embedding(self.num_decoder_tokens[i]+1, latent_dim,  name='s_emb{}'.format(i)) (syllabus_inputs[i])
+    x2, s_state_h, s_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True, name='lstm_post_emb{}'.format(i)) (x2)
+    syllabus_state_hs.append(s_state_h)
+    syllabus_state_cs.append(s_state_c)
+
+
+
+    x= Embedding(self.num_decoder_tokens[i]+1, latent_dim, mask_zero = True)  (aux_inputs[i]) # Get the embeddings of the decoder sequence
+    x, state_h2, state_c2 =  LSTM(latent_dim, return_sequences=True, return_state=True, name='lstm_post_line_emb{}'.format(i)) (x, initial_state=decoder_states_inputs)
+
+    if i == 0:
+        x, x_state_h, x_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True, name='lstm_line{}'.format(i)) (x,   initial_state=[state_h2 + syllabus_state_hs[i], state_c2 + syllabus_state_cs[i]])
+        last_states_hs.append(x_state_h)
+        last_states_cs.append(x_state_c)
+
+    else:
+        x, x_state_h, x_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True,name='lstm_line{}'.format(i)) (x,   initial_state=[state_h2 + last_states_hs[i-1]+syllabus_state_hs[i],state_c2 +  last_states_cs[i-1]+syllabus_state_cs[i]])
+        last_states_hs.append(x_state_h)
+        last_states_cs.append(x_state_c)
+
+    #print (x.shape)
+    decoder_outputs2.append(Dense(self.num_decoder_tokens[i], activation='softmax', name='predict{}'.format(i)) (x))
+
+
+#########
+
+
+for i in range(self.HAIKU_LINES_NUM):
+
+    x2 = Embedding(self.num_decoder_tokens[i]+1, latent_dim,  name='syllabus{}'.format(i)) (syllabus_inputs[i])
+    x2, s_state_h, s_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True, name='syllabus_lstm{}'.format(i)) (x2)
+    syllabus_state_hs.append(s_state_h)
+    syllabus_state_cs.append(s_state_c)
+
+    syllabus_dense.append( Dense(latent_dim , activation='softmax')  (syllabus_inputs[i]) )
+
+    x = Embedding(self.num_decoder_tokens[i]+1, latent_dim, mask_zero = True, name='line{}'.format(i)) (aux_inputs[i])
+    if i == 0:
+        x, x_state_h, x_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True, name='lstm{}'.format(i)) \
+            (x,   initial_state=[Add()([state_h , syllabus_dense[i]]), Add()([state_c , syllabus_dense[i]])])
+            #(x,   initial_state=[Add()([state_h , syllabus_state_hs[i]]), Add()([state_c , syllabus_state_cs[i]])])
+
+
+        #x, x_state_h, x_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True, name='lstm{}'.format(i)) (x,   initial_state=[Add([state_h , syllabus_state_hs[i]]), Add([state_c , syllabus_state_cs[i]])])
+
+        last_states_hs.append(x_state_h)
+        last_states_cs.append(x_state_c)
+
+    else:
+        #x, x_state_h, x_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True,name='lstm{}'.format(i)) (x,   initial_state=[state_h + last_states_hs[i-1]+syllabus_state_hs[i], state_c + last_states_cs[i-1]+syllabus_state_cs[i]])
+        x, x_state_h, x_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True,name='lstm{}'.format(i)) \
+            (x,   initial_state=[Add()([state_h , last_states_hs[i-1], syllabus_dense[i]]), Add()([state_c , last_states_cs[i-1], syllabus_dense[i]])])
+            #(x,   initial_state=[Add()([state_h , last_states_hs[i-1], syllabus_state_hs[i]]), Add()([state_c , last_states_cs[i-1], syllabus_state_cs[i]])])
+
+        last_states_hs.append(x_state_h)
+        last_states_cs.append(x_state_c)
+
+
+    outputs.append(Dense(self.num_decoder_tokens[i], activation='softmax', name='predict{}'.format(i)) (x))
+
+
+
+
+
+#########
+
+decoder_model = Model(
+    [aux_inputs] + decoder_states_inputs + [syllabus_inputs],
+    #[decoder_outputs2[0] , decoder_outputs2[1] , decoder_outputs2[2]] + [state_h2, state_c2])
+    [tuple(np.array(decoder_outputs2).tolist())] + [state_h2, state_c2])
+#decoder_model.load_weights(self.data_dir+'model8_weights.h5')
+#decoder_model.summary()
+
+print (seed)
--- train/train.py
+++ train/train.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-
 import numpy as np
 import pandas as pd
+import os
 
 Model = tf.keras.models.Model
 Input = tf.keras.layers.Input
@@ -11,7 +11,7 @@ Dense = tf.keras.layers.Dense
 concatenate = tf.keras.layers.concatenate
 Flatten = tf.keras.layers.Flatten
 Add =  tf.keras.layers.Add
-
+Constant = tf.keras.initializers.Constant
 pad_sequences = tf.keras.preprocessing.sequence.pad_sequences
 Tokenizer = tf.keras.preprocessing.text.Tokenizer
 
@@ -23,161 +23,103 @@ parser.add_argument("-o", "--out",dest="output",
                     help="location of model"
                     )
 
-parser.add_argument("-b", "--build",dest="build",
+parser.add_argument("-b", "--build",
                     help="build embedded matrix"
                     )
 
-modelDir = parser.parse_args().output
-build_matrix = parser.parse_args().build
+class Machine:
+    def __init__(self):
+        self.HAIKU_LINES_NUM = 3
+        self.dataDir='data/'
+        self.df = pd.read_csv(self.dataDir+'__INPUT.txt', sep = '\t')
+
+        self.modelDir = parser.parse_args().output
+        self.build_matrix = parser.parse_args().build if parser.parse_args().build  else None
+        self._setup()
+
+    @staticmethod
+    def _process_target_texts(text):
+        if 'start__ ' not in text:
+            return 'start__ '+text+' __end'
+
+    @staticmethod
+    def _arrayOfInt(texts):
+        try:
+            array = texts.split(',')
+            array = [int(i) for i in array]
+        except:
+            print (texts)
+        return array
+
+    def _setup(self):
+
+        self.df = self.df[:5000]
+        print (self.df.info())
+
+        t = Tokenizer()
+        t.fit_on_texts([self.df['input_texts'][i] for i in range(self.df.shape[0])])
+        vocab_size = len(t.word_index) + 1 # note - padded 1
+
+        encoded_docs = t.texts_to_sequences([self.df['input_texts'][i] for i in range(self.df.shape[0])])
+        max_len = max([len(l) for l in encoded_docs]) # get the max len
+        padded_docs = pad_sequences(encoded_docs, maxlen=max_len, padding='post')
+
+        if self.build_matrix:
+
+            embeddings_index = dict()
+            f = open(self.dataDir+'glove.6B/glove.6B.50d.txt') # try 50 dimension
+            for line in f:
+        		values = line.split()
+        		word = values[0]
+        		coefs = np.asarray(values[1:], dtype='float32')
+        		embeddings_index[word] = coefs
+            f.close()
+
+            embedding_matrix = np.zeros((vocab_size, 50)) # because we are using 50 dimension pre trained embedding
+            for word, i in t.word_index.items():
+                embedding_vector = embeddings_index.get(word)
+                if embedding_vector is not None:
+                    embedding_matrix[i] = embedding_vector
 
 
 
-dataDir='data/'
-HAIKU_LINES_NUM = 3
+        	
+        	np.savetxt(self.dataDir+'embedding_matrix.csv', embedding_matrix, delimiter=',')
+            print ('done building embedding matrix')
 
-df = pd.read_csv(dataDir+'__INPUT.txt', sep = '\t')
-df.info()
-
-df = df[:1000]
-t = Tokenizer()
-t.fit_on_texts([df['input_texts'][i] for i in range(df.shape[0])])
-vocab_size = len(t.word_index) + 1 # note - padded 1
-
-encoded_docs = t.texts_to_sequences([df['input_texts'][i] for i in range(df.shape[0])])
-max_len = max([len(l) for l in encoded_docs]) # get the max len
-padded_docs = pad_sequences(encoded_docs, maxlen=max_len, padding='post')
+        embedding_matrix = np.loadtxt(open(self.dataDir+'embedding_matrix.csv', "rb"), delimiter=",")
 
 
-# do this locally!
-if build_matrix:
 
-	embeddings_index = dict()
-	f = open(dataDir+'glove.6B/glove.6B.100d.txt') # try 100 dimension
-	for line in f:
-		values = line.split()
-		word = values[0]
-		coefs = np.asarray(values[1:], dtype='float32')
-		embeddings_index[word] = coefs
-	f.close()
-	print('Loaded %s word vectors.' % len(embeddings_index))
+        self.df.line1=self.df['line1'].apply(self._process_target_texts)
+        self.df.line2=self.df['line2'].apply(self._process_target_texts)
+        self.df.line3=self.df['line3'].apply(self._process_target_texts)
+        self.df.lib = self.df['lib'].apply(self._arrayOfInt)
 
-	embedding_matrix = np.zeros((vocab_size, 100)) # because we are using 100 dimension pre trained embedding
-	for word, i in t.word_index.items():
-		embedding_vector = embeddings_index.get(word)
-		if embedding_vector is not None:
-			embedding_matrix[i] = embedding_vector
+        targetTexts = [self.df.line1, self.df.line2, self.df.line3 ]
+        target_words = [ set(' '.join(item).split(' ')) for item in targetTexts ]
 
-	print (embedding_matrix)
-	print (type(embedding_matrix))
-	print (embedding_matrix.shape)
+        num_encoder_tokens = vocab_size
+        num_decoder_tokens = [len(i) for i in target_words ]
 
+        for i in range(self.HAIKU_LINES_NUM):
+            num_decoder_tokens[i] += 1
 
-	from numpy import savetxt, loadtxt
-	savetxt(dataDir+'embedding_matrix.csv', embedding_matrix, delimiter=',')
+        target_token_index = [dict( [(char, i+1) for i, char in enumerate(j)]) for j in target_words]
+        reverse_target_char_index = [dict((i, word) for word, i in j.items()) for j in target_token_index ]
 
+        max_encoder_seq_length = max_len
+        max_decoder_seq_length = [ max ([len(column[i].split(' ')) for i in range(self.df.shape[0])]) for column in targetTexts]
 
-embedding_matrix = np.loadtxt(open(dataDir+'embedding_matrix.csv', "rb"), delimiter=",")
+        input_token_index = t.word_index
+        reverse_input_char_index = dict(map(reversed, t.word_index.items()))
 
+        y = np.array( targetTexts)
 
-def _process_target_texts(text):
-    if 'start__ ' not in text:
-        return 'start__ '+text+' __end'
+        self.y = y.transpose()
+        self.X = (padded_docs , self.df.lib)
 
-df.line1=df['line1'].apply(_process_target_texts)
-df.line2=df['line2'].apply(_process_target_texts)
-df.line3=df['line3'].apply(_process_target_texts)
-
-def _arrayOfInt(texts):
-    try:
-        array = texts.split(',')
-        array = [int(i) for i in array]
-    except:
-        print (texts)
-    return array
-
-df.lib = df['lib'].apply(_arrayOfInt)
-
-targetTexts = [df.line1, df.line2, df.line3 ]
-target_words = [ set(' '.join(item).split(' ')) for item in targetTexts ]
-
-num_encoder_tokens = vocab_size
-num_decoder_tokens = [len(i) for i in target_words ]
-
-for i in range(HAIKU_LINES_NUM):
-    num_decoder_tokens[i] += 1
-
-target_token_index = [dict( [(char, i+1) for i, char in enumerate(j)]) for j in target_words]
-reverse_target_char_index = [dict((i, word) for word, i in j.items()) for j in target_token_index ]
-
-max_encoder_seq_length = max_len
-max_decoder_seq_length = [ max ([len(column[i].split(' ')) for i in range(df.shape[0])]) for column in targetTexts]
-
-input_token_index = t.word_index
-reverse_input_char_index = dict(map(reversed, t.word_index.items()))
-
-y = np.array( targetTexts)
-y = y.transpose()
-X = (padded_docs , df.lib)
-
-epochs = 1
-latent_dim = 100
-dropout=0.1 #regularization , to prevent over fitting
-learning_rate = 0.0025
-optimizer = 'rmsprop'
-lstm_dim =latent_dim
-
-inputs = Input(shape=(max_len,))
-
-# from tensorflow.contrib.keras.api.keras.initializers import Constant
-# n = Embedding(2, 2, embeddings_initializer=Constant(m), input_length=1, name='embedding_matrix_1', trainable=False)
-
-# Constant = tf.compat.v1.keras.initializers.Constant
-Constant = tf.keras.initializers.Constant
-x =  Embedding(num_encoder_tokens, latent_dim,  embeddings_initializer=Constant(embedding_matrix), input_length=max_len, trainable=False, mask_zero = True)(inputs)
-# x =  Embedding(num_encoder_tokens, latent_dim,  weights=[embedding_matrix], input_length=max_len, trainable=False, mask_zero = True)(inputs)
-_ , state_h, state_c = LSTM(latent_dim,  return_state=True) (x)
-
-
-aux_inputs = [Input(shape=(None,), name='aux_input_{}'.format(i)) for i in range(HAIKU_LINES_NUM)]
-
-syllabus_inputs = [Input(shape=(1,), name='syllabus_input_{}'.format(i)) for i in range(HAIKU_LINES_NUM)] # assume training data has 12 types of lines
-
-
-syllabus_dense = []
-
-last_states_hs = []
-last_states_cs = []
-
-outputs = []
-
-for i in range(HAIKU_LINES_NUM):
-
-    syllabus_dense.append( Dense(latent_dim , activation='softmax')  (syllabus_inputs[i]) )
-    x = Embedding(num_decoder_tokens[i]+1, latent_dim, mask_zero = True, name='line{}'.format(i)) (aux_inputs[i])
-    if i == 0:
-        x, x_state_h, x_state_c = LSTM(lstm_dim , return_sequences=True,  return_state=True, name='lstm{}'.format(i)) \
-            (x,   initial_state=[Add()([state_h , syllabus_dense[i]]), Add()([state_c , syllabus_dense[i]])])
-
-        last_states_hs.append(x_state_h)
-        last_states_cs.append(x_state_c)
-
-    else:
-        x, x_state_h, x_state_c = LSTM(lstm_dim , return_sequences=True,  return_state=True,name='lstm{}'.format(i)) \
-            (x,   initial_state=[Add()([state_h , last_states_hs[i-1], syllabus_dense[i]]), Add()([state_c , last_states_cs[i-1], syllabus_dense[i]])])
-
-        last_states_hs.append(x_state_h)
-        last_states_cs.append(x_state_c)
-
-
-    outputs.append(Dense(num_decoder_tokens[i], activation='softmax', name='predict{}'.format(i)) (x))
-
-#model = Model([inputs, tuple(np.array(aux_inputs).tolist()), tuple(np.array(syllabus_inputs).tolist())], [tuple(np.array(outputs).tolist())], name='machine')
-model = Model([inputs, aux_inputs[0],aux_inputs[1],aux_inputs[2], syllabus_inputs[0],syllabus_inputs[1],syllabus_inputs[2]], [outputs[0],outputs[1],outputs[2]], name='machine')
-
-model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-model.summary()
-
-params ={
+        self.params ={
             'num_encoder_tokens':num_encoder_tokens,
             'num_decoder_tokens':num_decoder_tokens,
             'max_encoder_seq_length':max_encoder_seq_length,
@@ -186,24 +128,96 @@ params ={
             'reverse_input_char_index':reverse_input_char_index,
             'target_token_index':target_token_index,
             'reverse_target_char_index':reverse_target_char_index,
-            'number_of_output':HAIKU_LINES_NUM,
-	    'embedding_matrix':embedding_matrix,
-            #'latent_dim':latent_dim,
-            #'num_syllabus':self.num_syllabus = len(syllabus)
-        }
-
-import r
-DataGenerator = r.DataGenerator
-
-training_generator = DataGenerator(X, y, params, batch_size=256 )
-validation_generator = DataGenerator(X, y, params, batch_size=256)
+            'number_of_output':self.HAIKU_LINES_NUM,
+            'HAIKU_LINES_NUM':self.HAIKU_LINES_NUM,
+	    	'embedding_matrix':embedding_matrix,
+                        #'latent_dim':latent_dim,
+                        #'num_syllabus':self.num_syllabus = len(syllabus)
+            }
 
 
-history = model.fit(training_generator, validation_data=validation_generator,  epochs=epochs, )
-#history = model.fit_generator(training_generator, validation_data=validation_generator,  epochs=epochs, use_multiprocessing=True,)
+    def train(self, epochs=5):
 
-modelDir = modelDir if modelDir else './model/haiku/'
-model.save(modelDir+'modelv2-1.h5')
 
-print (history.history)
-print ('model saved in {}'.format(modelDir))
+
+        latent_dim = self.params['embedding_matrix'].shape[1]
+        dropout=0.1 #regularization , to prevent over fitting
+        learning_rate = 0.0025
+
+        import sys, os
+        python_version = 2 if '2.' in sys.version.split('|')[0] else 3
+
+        if python_version == 2: optimizer = 'rmsprop'
+        else: optimizer =  tf.keras.optimizers.RMSprop(learning_rate=learning_rate, rho=0.9)
+
+        # Model Architecture
+        inputs = Input(shape=(self.params['max_encoder_seq_length'],))
+
+        x =  Embedding(self.params['num_encoder_tokens']+1, latent_dim,  embeddings_initializer=Constant(self.params['embedding_matrix']), \
+        input_length=self.params['max_encoder_seq_length'], trainable=False, mask_zero = True)(inputs)
+
+        _ , state_h, state_c = LSTM(latent_dim,  return_state=True) (x)
+
+
+        aux_inputs = [Input(shape=(None,), name='aux_input_{}'.format(i)) for i in range(self.params['HAIKU_LINES_NUM'])]
+
+        syllabus_inputs = [Input(shape=(1,), name='syllabus_input_{}'.format(i)) for i in range(self.params['HAIKU_LINES_NUM'])]
+
+        syllabus_dense = []
+        last_states_hs = []
+        last_states_cs = []
+        outputs = []
+
+        for i in range(self.params['HAIKU_LINES_NUM']):
+
+            syllabus_dense.append( Dense(latent_dim , activation='softmax')  (syllabus_inputs[i]) )
+            x = Embedding(self.params['num_decoder_tokens'][i]+1, latent_dim, mask_zero = True, name='line{}'.format(i)) (aux_inputs[i])
+            if i == 0:
+                x, x_state_h, x_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True, name='lstm{}'.format(i)) \
+                    (x,   initial_state=[Add()([state_h , syllabus_dense[i]]), Add()([state_c , syllabus_dense[i]])])
+
+                last_states_hs.append(x_state_h)
+                last_states_cs.append(x_state_c)
+
+            else:
+                x, x_state_h, x_state_c = LSTM(latent_dim , return_sequences=True,  return_state=True,name='lstm{}'.format(i)) \
+                    (x,   initial_state=[Add()([state_h , last_states_hs[i-1], syllabus_dense[i]]), Add()([state_c , last_states_cs[i-1], syllabus_dense[i]])])
+
+                last_states_hs.append(x_state_h)
+                last_states_cs.append(x_state_c)
+
+
+            outputs.append(Dense(self.params['num_decoder_tokens'][i], activation='softmax', name='predict{}'.format(i)) (x))
+
+        #model = Model([inputs, tuple(np.array(aux_inputs).tolist()), tuple(np.array(syllabus_inputs).tolist())], [tuple(np.array(outputs).tolist())], name='machine')
+        model = Model([inputs, aux_inputs[0],aux_inputs[1],aux_inputs[2], syllabus_inputs[0],syllabus_inputs[1],syllabus_inputs[2]], [outputs[0],outputs[1],outputs[2]], name='machine')
+
+        model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+        model.summary()
+
+
+        from r import DataGenerator
+        #import r
+        #DataGenerator = r.DataGenerator
+
+        training_generator = DataGenerator(self.X, self.y, self.params, batch_size=256 )
+        validation_generator = DataGenerator(self.X, self.y, self.params, batch_size=256)
+
+
+        #history = model.fit(training_generator, validation_data=validation_generator,  epochs=epochs, )
+        history = model.fit_generator(training_generator, validation_data=validation_generator,  epochs=epochs, use_multiprocessing=True,)
+
+        self.modelDir = self.modelDir if self.modelDir else './model/haiku/'
+        if not os.path.exists(self.modelDir):
+            os.makedirs(self.modelDir)
+        model.save(self.modelDir+'modelv2-3.h5')
+        #model.save_weights(modelDir+'modelv2_weights.h5')
+
+
+
+        return history
+
+if __name__ == "__main__":
+    haiku = Machine()
+    h = haiku.train(epochs=3)
+    print (h.history)
